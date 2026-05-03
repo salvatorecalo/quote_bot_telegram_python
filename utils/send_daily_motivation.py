78bd6fb0fd60fbd518bda2b30a0f824d295852cd
salvo <dafder45678@gmail.com>
2026-05-03 11:49:51 +0200

    fix: renamed variabled with conventions

--- utils/send_daily_motivation.py
+++ utils/send_daily_motivation.py
@@ -2,7 +2,7 @@ from telegram.ext import ContextTypes
 from .get_all_user import get_all_users
 from random import randint
 
-quotes = [
+QUOTES = [
     "So che ce la puoi fare campione ",
     "Non mollare, sei più forte di quanto pensi!",
     "Ogni giorno è una nuova opportunità per crescere e migliorare.",
@@ -11,10 +11,7 @@ quotes = [
 ]
 async def send_daily_motivation(context: ContextTypes.DEFAULT_TYPE):
     """Sends a daily motivational quote to the user."""
-    quote = randint(0, len(quotes) - 1)
+    random_index = randint(0, len(QUOTES) - 1)
     all_users = get_all_users()
     for user_id in all_users:
-        await context.bot.send_message(chat_id=user_id, text=quotes[quote])
-
-    quote = "Believe you can and you're halfway there. - Theodore Roosevelt"
-    
+        await context.bot.send_message(chat_id=user_id, text=QUOTES[random_index])    
