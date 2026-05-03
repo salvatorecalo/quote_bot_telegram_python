503c6cd09fcb0df57d96ae30efc0425b73339522
salvo <dafder45678@gmail.com>
2026-05-03 11:59:26 +0200

    fix: removed not found method app.run_server

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def home():
 
 def run_bot():
     port = int(os.environ.get("PORT", 5000))
-    app.run_server(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
 
 Thread(target=run_bot).start()
 
