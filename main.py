cefc3cf244231fc9032118e3722596574e2efd20
salvo <dafder45678@gmail.com>
2026-05-03 11:54:39 +0200

    feat: add flask just because leapcell want a web server

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+from threading import Thread
+
 from utils import setup_logger, handle_callback, start_command, send_daily_motivation, create_user_table
 from telegram.ext import ApplicationBuilder, CallbackQueryHandler, CommandHandler
 from dotenv import load_dotenv
 import os
 import datetime
 import pytz
+# I import flask just because leapcell is looking for a web server to run the bot :(
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, this is the home page of the bot!"
+
+def run_bot():
+    port = int(os.environ.get("PORT", 5000))
+    app.run_server(host="0.0.0.0", port=port)
+
+Thread(target=run_bot).start()
+
+# Here we put our basic bot stuff
 load_dotenv()
 logger = setup_logger(__name__)
 
@@ -18,7 +35,7 @@ def main():
         app.add_handler(CommandHandler("start", start_command))
         
         job_queue = app.job_queue
-        hours = datetime.time(hour=22, minute=31, second=0, tzinfo=pytz.timezone('Europe/Rome'))
+        hours = datetime.time(hour=11, minute=48, second=0, tzinfo=pytz.timezone('Europe/Rome'))
         job_queue.run_daily(send_daily_motivation, time=hours)
         
         app.run_polling()
